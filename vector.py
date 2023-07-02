992d202c24a6fd4724ce73285702af67ccdd9048
Artur Garcia <arturgarcia046@gmail.com>
2023-07-02 11:41:40 -0300

    resolved

--- vector.py
+++ vector.py
@@ -1,16 +1,16 @@
 """Módulo com as funções de manipulação de vetores."""
-from tipos import Vetor, Escalar, Matriz
+from tipos import Escalar, Matriz, Vetor
+
 
 def norma(x: Vetor) -> Escalar | None:
     """Calcula a norma de um vetor"""
     if x == []:
         return None
-    
-    sum = float()
+    soma_quadrados = 0.0
 
     for ele in x:
-        sum += ele ** 2
-    result = sum**(1 / 2)
+        soma_quadrados += ele**2
+    result = soma_quadrados ** (1 / 2)
     return result
 
 
@@ -18,41 +18,61 @@ def soma(x: Vetor, y: Vetor) -> Vetor | None:
     """Soma dois vetores"""
     if len(x) != len(y):
         return None
-    
-    result = [0 for _ in range(x)]
+    if x == [] and y == []:
+        return []
+
+    result = [0 for _ in range(len(x))]
 
     for i, ele_x in enumerate(x):
         result[i] = ele_x + y[i]
+    return result
 
 
-def multiplicação_por_escalar(vetor: Vetor, escalar: Escalar) -> Vetor:
+def multiplicacao_por_escalar(vetor: Vetor, escalar: Escalar) -> Vetor:
     """Multiplica um vetor por um escalar"""
-    # TODO: implementar
-    # a multiplicação de um vetor [1, 2, 4] por um escalar 2 é [2, 4, 8]
-    
+    vetor_res = [0] * len(vetor)
+
+    for i, ele in enumerate(vetor):
+        vetor_res[i] = ele * escalar
+    return vetor_res
 
 
 def produto_interno(x: Vetor, y: Vetor) -> Escalar | None:
     """Calcula o produto interno de dois vetores"""
-    # TODO: implementar
-    # o produto interno de dois vetores [1, 2, 4] e [2, 3, 4] é 24
-    # o produto interno consiste em multiplicar os elementos de mesmo índice e somar os resultados
-    # a multiplicação só pode ser realizada se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
-    # caso os vetores sejam vazios o resultado é 0
+    if len(x) != len(y):
+        return None
+    if x == [] and y == []:
+        return 0
+
+    result = int()
+
+    for i, elex in enumerate(x):
+        result += elex * y[i]
+    return result
 
 
 def produto_vetorial(x: Vetor, y: Vetor) -> Vetor | None:
     """Calcula o produto vetorial de dois vetores"""
-    # TODO: implementar
-    # o produto vetorial de dois vetores [1, 2, 4] e [2, 3, 4] é [-4, 4, -1]
-    # o produto vetorial só pode ser realizado se os vetores tem 3 elementos.
-    # caso contrário, deve retornar None
+    if len(x) != 3 or len(y) != 3:
+        return None
+    result = [0] * len(x)
+    for i in range(3):
+        result[i] = x[(i + 1) % 3] * y[(i + 2) % 3] - x[(i + 2) % 3] * y[(i + 1) % 3]
+
+    return result
 
 
-def produto_diádico(x: Vetor, y: Vetor) -> Matriz | None:
+def produto_diadico(x: Vetor, y: Vetor) -> Matriz | None:
     """Calcula o produto diádico de dois vetores"""
-    # TODO: implementar
-    # o produto diádico de dois vetores [1, 2, 4] e [2, 3, 4] é [[2, 3, 4], [4, 6, 8], [8, 12, 16]]
-    # o produto diádico só pode ser realizado se os vetores tem a mesma quantidade de elementos.
-    # caso contrário, deve retornar None
+    if len(x) != len(y):
+        return None
+
+    result = []
+
+    for elex in x:
+        linha = []
+        for eley in y:
+            linha.append(elex * eley)
+        result.append(linha)
+
+    return result
